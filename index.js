2f5f5cb6f9a1b6e18061eee0b4aa83af9f980937
BagyaShivani <bagya.shivani@wipro.com>
2020-06-11 10:13:41 +0000

    Intial commit by simplegit

---
+++ index.js
@@ -0,0 +1,43 @@
+const simpleGit = require('simple-git')();
+// Shelljs package for running shell tasks optional
+const shellJs = require('shelljs');
+// Simple Git with Promise for handling success and failure
+const simpleGitPromise = require('simple-git/promise')();
+
+// change current directory to repo directory in local
+shellJs.cd('C:/Users/ca_user1/Desktop/automatejs-master');
+// Repo name
+const repo = 'dummy';  //Repo name
+// User name and password of your GitHub
+const userName = 'BagyaShivani';
+const password = 'shivani2904';
+// Set up GitHub url like this so no manual entry of user pass needed
+const gitHubUrl = 'https://BagyaShivani:shivani2904@github.com/BagyaShivani/dummy';
+// add local git config like username and email
+simpleGit.addConfig('user.email','bagya.shivani@wipro.com');
+simpleGit.addConfig('user.name','BagyaShivani');
+// Add remore repo url as origin to repo
+simpleGitPromise.addRemote('origin',gitHubUrl);
+// Add all files for commit
+  simpleGitPromise.add('.')
+    .then(
+       (addSuccess) => {
+          console.log(addSuccess);
+       }, (failedAdd) => {
+          console.log('adding files failed');
+    });
+// Commit files as Initial Commit
+ simpleGitPromise.commit('Intial commit by simplegit')
+   .then(
+      (successCommit) => {
+        console.log(successCommit);
+     }, (failed) => {
+        console.log('failed commmit');
+ });
+// Finally push to online repository
+ simpleGitPromise.push('origin','master')
+    .then((success) => {
+       console.log('repo successfully pushed');
+    },(failed)=> {
+       console.log('repo push failed');
+ });
