5f854d9ebb4a7318b9ae4805e8fb40424784de59
BagyaShivani <bagya.shivani@wipro.com>
2020-06-15 09:19:27 +0000

    Intial commit by simplegit

--- index.js
+++ index.js
@@ -6,21 +6,43 @@ const simpleGitPromise = require('simple-git/promise')();
 // change current directory to repo directory in local
 
 shellJs.cd('C:/Users/ca_user1/Desktop/automatejs-master');
+let folder=process.argv[3];
 
 // Repo name
-const repo = 'dummy';  //Repo name
+//const repo = 'dum';  //Repo name
 // User name and password of your GitHub
 const userName = 'BagyaShivani';
 const password = 'shivani2904';
 //Set up GitHub url like this so no manual entry of user pass needed
 
-const gitHubUrl = `https://${userName}:${password}@github.com/${userName}/${repo}`;
+const gitHubUrl = `https://${userName}:${password}@github.com/${userName}/${folder}`;
 // add local git config like username and email
 simpleGit.addConfig('user.email','bagya.shivani@wipro.com');
 simpleGit.addConfig('user.name','BagyaShivani');
-//simpleGitPromise.removeRemote('origin',gitHubUrl);
-if( process.argv[2]==='push')
+const {Octokit}  = require("@octokit/rest");
+const octo = new Octokit({
+   auth: "537fed6eed759d5b078830ddef9b2d92f1e2f78a"
+ });
+if(process.argv[2]==='create')
 {
+   
+   octo.repos.createForAuthenticatedUser({
+      name: folder
+  }).then(data => {
+      console.log("successfully created repo " + folder)
+  }).catch(e => {
+
+      console.log(e);
+      
+   });
+
+   
+   
+}
+else if( process.argv[2]==='push')
+{
+   simpleGitPromise.removeRemote('origin',gitHubUrl);
+
 // Add remore repo url as origin to repo
 simpleGitPromise.addRemote('origin',gitHubUrl);
 // Add all files for commit
